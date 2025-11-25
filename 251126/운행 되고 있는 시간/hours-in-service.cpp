eb5781d7c38afae029bc4f2d6f5734fca2d98d98
bornagainer91 <bornagainer91@gmail.com>
2025-11-26 08:44:30 +0900

    Title: 운행 되고 있는 시간, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251126/운행 되고 있는 시간/hours-in-service.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+int N {};
+int A[101] {}, B[101] {};
+
+int main() {
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A[i] >> B[i];
+    }
+
+    int iResult = INT_MIN;    
+
+    for (int i=0; i<N; i++)
+    {        
+        int iWorkTime {};
+        int iStack[1001] {};
+
+        for (int j=0; j<N; j++)
+        {
+            if (i==j) continue;
+
+            for (int k=A[j]; k<B[j]; k++)
+            {
+                iStack[k]++;
+            }
+        }
+
+        for (int j=1; j<1001; j++)
+        {
+            if (iStack[j] > 0) // 여러멍이 일해도(겹쳐도) 1번 카운트 됨으로 문제 없음.
+            {
+                iWorkTime++;
+            }
+        }
+
+        // cout << i+1 <<"번째: " << iWorkTime << endl;
+        iResult = max(iResult, iWorkTime);
+    }
+
+    cout << iResult << endl;
+
+    return 0;
+}
