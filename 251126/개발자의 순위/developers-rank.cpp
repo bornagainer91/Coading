45270a3a6c913cfde5ccade67d55f209cb3d445a
bornagainer91 <bornagainer91@gmail.com>
2025-11-26 13:21:08 +0900

    Title: 개발자의 순위, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251126/개발자의 순위/developers-rank.cpp
+++ 251126/개발자의 순위/developers-rank.cpp
@@ -1,59 +1,114 @@
+// #include <iostream>
+// #include <algorithm>
+// #include <climits>
+
+// #define MAX_K 10
+// #define MAX_N 20
+
+// using namespace std;
+
+// int k {}, n {};
+// int arr[MAX_K][MAX_N+1] {};
+// int iRank[MAX_K][MAX_N+1] {};
+
+// int main()
+// {
+//     cin >> k >> n;
+
+//     for (int i = 0; i < k; i++)
+//     {
+//         // cout << i+1 << "번째: " << endl;
+
+//         for (int j = 0; j < n; j++)
+//         {
+//             cin >> arr[i][j];
+//             int iDeveloper = arr[i][j];
+//             iRank[i][iDeveloper] = j;
+//             // cout << iDeveloper << "의 순위: " << iRank[i][iDeveloper] << endl;
+//         }
+//         // cout << endl;
+//     }
+
+//     int answer = 0;
+
+//     for (int i = 1; i <= n; i++)
+//     {
+//         for (int j = 1; j <= n; j++)
+//         {
+//             if (i == j) continue;
+
+//             bool ok = true;
+
+//             // 모든 경기에서 a가 b보다 높은 순위인지 체크
+//             // 숫자가 겹치지 않는다고 했으니깐 같다는 표시는 안해도 됨.
+//             for (int l = 0; l < k; l++)
+//             {
+//                 if (iRank[l][i] >= iRank[l][j])
+//                 {
+//                     ok = false;
+//                     break;
+//                 }
+//             }
+
+//             if (ok) answer++;
+//         }
+//     }
+
+//     cout << answer << endl;
+
+//     return 0;
+// }
+
 #include <iostream>
-#include <algorithm>
-#include <climits>
 
 #define MAX_K 10
 #define MAX_N 20
 
 using namespace std;
 
-int k {}, n {};
+int N {}, K {};
 int arr[MAX_K][MAX_N+1] {};
 int iRank[MAX_K][MAX_N+1] {};
 
 int main()
 {
-    cin >> k >> n;
+    cin >> N >> K;
 
-    for (int i = 0; i < k; i++)
+    for (int i=0; i<N; i++)
     {
-        // cout << i+1 << "번째: " << endl;
-
-        for (int j = 0; j < n; j++)
+        for (int j=0; j<K; j++)
         {
             cin >> arr[i][j];
             int iDeveloper = arr[i][j];
             iRank[i][iDeveloper] = j;
-            // cout << iDeveloper << "의 순위: " << iRank[i][iDeveloper] << endl;
         }
-        // cout << endl;
     }
 
-    int answer = 0;
+    int iCnt {};
 
-    for (int i = 1; i <= n; i++)
+    for (int a=1; a<=K; a++)
     {
-        for (int j = 1; j <= n; j++)
+        for (int b=1; b<=K; b++)
         {
-            if (i == j) continue;
+            int bChk = true;
+            // a,b가 같으면 넘어가기. 사람이 같으면 순위를 비교할 필요가 없음.
+            if (a==b) continue;
 
-            bool ok = true;
-
-            // 모든 경기에서 a가 b보다 높은 순위인지 체크
-            for (int l = 0; l < k; l++)
+            // a,b가 i번째 경기마다 순위가 어떠한지 체크. 한번이라도 순위가 벗어나면 탈출
+            for (int i=0; i<N; i++)
             {
-                if (iRank[l][i] <= iRank[l][j])
+                // a가 b보다 크거나 같으면 해제(순위가 작을수록 높기 때문에. 순위가 뒤쳐지면 false임)
+                if (iRank[i][a] >= iRank[i][b])
                 {
-                    ok = false;
+                    bChk = false;
                     break;
-                }
+                }                
             }
-
-            if (ok) answer++;
+            if(bChk) iCnt++;
         }
     }
 
-    cout << answer << endl;
+    cout << iCnt << endl;
 
     return 0;
 }
