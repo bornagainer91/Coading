59ba614c1ca4575cd98cc631fa1f4b3a40d364e1
bornagainer91 <bornagainer91@gmail.com>
2025-11-28 00:17:54 +0900

    Title: 숫자들의 합 중 최대, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 251128/숫자들의 합 중 최대/maximum-of-sum-of-numbers.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+int X, Y;
+
+int main()
+{
+    cin >> X >> Y;
+
+    int iMax = INT_MIN;
+
+    for (int i=X; i<=Y; i++)
+    {
+        if (i < 10)
+        if (i < 10)
+        if (i < 10)
+    }
+
+    cout << iMax << endl;
+
+    return 0;
+}
