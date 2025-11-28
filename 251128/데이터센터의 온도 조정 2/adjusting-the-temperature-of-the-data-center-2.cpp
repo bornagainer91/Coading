16d8183d55b00f6bd9cf129ada5f1bd4d96d4eb4
bornagainer91 <bornagainer91@gmail.com>
2025-11-28 10:56:25 +0900

    Title: 데이터센터의 온도 조정 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251128/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <climits>
+#include <algorithm>
+
+using namespace std;
+
+/*
+첫번째 줄 - N, C, G, H
+N - 장비의 개수
+C, G, H - 장비들의 작업량
+
+두번째 줄 - Ta, Tb
+Ta, Tb - 온도 범위
+*/
+
+int N {}, C {}, G {}, H {};
+int Ta[1001] {}, Tb[1001] {};
+
+int CaseMachine(int iMachineNum, int iTemperNum)
+{
+    if (iTemperNum < Ta[iMachineNum])
+    {
+        return C;
+    }
+    
+    if (iTemperNum >= Ta[iMachineNum] && iTemperNum <= Tb[iMachineNum])
+    {
+        return G;
+    }
+    
+    if (iTemperNum > Tb[iMachineNum])
+    {
+        return H;
+    }
+    
+    return 0;
+    
+}
+
+int main()
+{
+    cin >> N >> C >> G >> H;    
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> Ta[i] >> Tb[i];
+    }
+
+    int iTotalMaxTemp {};
+    
+    for (int j=0; j<=1000; j++)
+    {
+        int iSumTemp {};
+        for (int i = 0; i < N; i++)
+        {
+            iSumTemp += CaseMachine(i, j);
+        }
+
+        iTotalMaxTemp = max(iTotalMaxTemp, iSumTemp);
+        // cout << j+1 << "도 일때 최고 작업량: " << iTotalMaxTemp << endl;
+    }
+
+    cout << iTotalMaxTemp << endl;
+
+    return 0;
+}
